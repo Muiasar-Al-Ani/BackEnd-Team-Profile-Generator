02bfd54073efebff192c4585401be4a59f8a7837
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-08 19:21:20 -0400

    Added all the testing for the Engineer class

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -11,48 +11,48 @@ describe("Employee class", () => {
 
     it("should check if the given employee name gets processed properly through the constructor", () => {
       const name = "Muiasar";
-      const fakeEmployee = new Employee(name);
-      expect(fakeEmployee.name).toEqual(name);
+      const newEmployee = new Employee(name);
+      expect(newEmployee.name).toEqual(name);
     });
     it("should check if the given employee Id gets processed properly through the constructor", () => {
       const name = "Muiasar";
-      const fakeID = "12345";
-      const fakeEmployee = new Employee(name, fakeID);
-      expect(fakeEmployee.id).toEqual(fakeID);
+      const newID = "12345";
+      const newEmployee = new Employee(name, newID);
+      expect(newEmployee.id).toEqual(newID);
     });
     it("should check if the given employee email gets processed properly through the constructor", () => {
       const name = "Muiasar";
-      const fakeID = "12345";
+      const newID = "12345";
       const myEmail = "muiasar2012@gmail.com";
-      const fakeEmployee = new Employee(name, fakeID, myEmail);
-      expect(fakeEmployee.email).toEqual(myEmail);
+      const newEmployee = new Employee(name, newID, myEmail);
+      expect(newEmployee.email).toEqual(myEmail);
     });
   });
-  describe("get functions", () => {
+  describe("The get functions", () => {
     it("getName() should return the name of the employee", () => {
       const name = "Muiasar";
-      const fakeEmployee = new Employee(name);
-      expect(fakeEmployee.getName()).toEqual(name);
+      const newEmployee = new Employee(name);
+      expect(newEmployee.getName()).toEqual(name);
     });
     it("getId() should return the ID of the employee", () => {
       const name = "Muiasar";
-      const fakeID = "12345";
-      const fakeEmployee = new Employee(name, fakeID);
-      expect(fakeEmployee.getId()).toEqual(fakeID);
+      const newID = "12345";
+      const newEmployee = new Employee(name, newID);
+      expect(newEmployee.getId()).toEqual(newID);
     });
     it("getEmail() should return the email of the employee", () => {
       const name = "Muiasar";
-      const fakeID = "12345";
+      const newID = "12345";
       const myEmail = "muiasar2012@gmail.com";
-      const fakeEmployee = new Employee(name, fakeID, myEmail);
-      expect(fakeEmployee.getEmail()).toEqual(myEmail);
+      const newEmployee = new Employee(name, newID, myEmail);
+      expect(newEmployee.getEmail()).toEqual(myEmail);
     });
     it("getRole() should return the Role of the employee", () => {
       const name = "Muiasar";
-      const fakeID = "12345";
+      const newID = "12345";
       const myEmail = "muiasar2012@gmail.com";
-      const fakeEmployee = new Employee(name, fakeID, myEmail);
-      expect(fakeEmployee.getRole()).toEqual("Employee");
+      const newEmployee = new Employee(name, newID, myEmail);
+      expect(newEmployee.getRole()).toEqual("Employee");
     });
   });
 });
--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,2 +1,46 @@
-// Imports the Employee class
-const Engineer = require("./lib/Engineer");
+// Imports the Engineer class
+const Engineer = require("../lib/Engineer");
+
+// Tests the constructor class of Engineer
+describe("Engineer class", () => {
+  describe("initialization", () => {
+    it("should check if the initialized object is an instance of the Engineer class", () => {
+      const newEngineer = new Engineer();
+      expect(newEngineer instanceof Engineer).toEqual(true);
+    });
+    it("should check if the given employee name, ID, email, and the GitHub user name gets processed properly through the constructor", () => {
+      const name = "Muiasar";
+      const newID = "12345";
+      const myEmail = "muiasar2012@gmail.com";
+      const gitHubUserName = "Muiasar-Al-Ani";
+
+      const newEngineer = new Engineer(name, newID, myEmail, gitHubUserName);
+
+      expect(newEngineer.name).toEqual(name);
+      expect(newEngineer.id).toEqual(newID);
+      expect(newEngineer.email).toEqual(myEmail);
+      expect(newEngineer.gitHubUserName).toEqual(gitHubUserName);
+    });
+  });
+  describe("The get functions", () => {
+    it("getRole() should return the Role of the Engineer", () => {
+      const name = "Muiasar";
+      const newID = "12345";
+      const myEmail = "muiasar2012@gmail.com";
+
+      const newEngineer = new Engineer(name, newID, myEmail);
+
+      expect(newEngineer.getRole()).toEqual("Engineer");
+    });
+    it("getGitHub() should return the GitHub user name of the Engineer", () => {
+      const name = "Muiasar";
+      const newID = "12345";
+      const myEmail = "muiasar2012@gmail.com";
+      const gitHubUserName = "Muiasar-Al-Ani";
+
+      const newEngineer = new Engineer(name, newID, myEmail, gitHubUserName);
+
+      expect(newEngineer.getGithub()).toEqual(gitHubUserName);
+    });
+  });
+});
